1ffd336c4dadcc6ac1f8a8b904a4fc8439989d16
Davi Lucas <davilucasdlcreator@gmail.com>
2021-02-16 20:28:31 -0300

    :sparkles: feat api v1

---
+++ pages/api/v1/animatronic/[name].js
@@ -0,0 +1,21 @@
+import animatronics from '../animatronics.json';
+
+export default function getOneCharacter(request,response){
+    const { name } = request.query;
+    if (request.method === 'OPTIONS') {
+        response.status(200).end();
+        return;
+    }
+    
+    response.setHeader('Access-Control-Allow-Credentials', true);
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    
+    const character = animatronics.find(animatronic => animatronic.name.trim().toLowerCase() === name.trim().toLowerCase())
+    if(!character){
+        response.status(404).json({ message:'That animatronic doesnt exists',name })
+        return;
+    }
+
+    response.status(200).json(animatronics);
+}
