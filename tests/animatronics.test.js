70eb671de0d6c1cc5590b696d0b9c3678be910a1
Davi Lucas <davilucasdlcreator@gmail.com>
2021-02-24 20:14:44 -0300

    :sparkles: feat tests

---
+++ tests/animatronics.test.js
@@ -0,0 +1,36 @@
+const { describe, it } = require("mocha");
+const assert = require("assert");
+const animatronics = require("../pages/api/v1/animatronics.json");
+
+describe("Animatronics tests", () => {
+  it("Verify if animatronics are an array", () => {
+    assert.ok(Array.isArray(animatronics));
+  });
+  it("Verify if all the animatronics have these values:name,image and game", () => {
+    const allAnimatronicsHaveTheCorrectValues = animatronics.every(
+      (animatronic) => Object.keys(animatronic).join("-") === "name-image-game"
+    );
+    assert.ok(allAnimatronicsHaveTheCorrectValues);
+  });
+  it("Verify if all the animatronics have the name with the first letter capitalize", () => {
+    const allAnimatronicsHaveTheNameWithTheFirstLetterCapitalize = animatronics.every(
+      (animatronic) => animatronic.name[0] === animatronic.name[0].toUpperCase()
+    );
+    assert.ok(allAnimatronicsHaveTheNameWithTheFirstLetterCapitalize);
+  });
+  it("Verify if all the images keys are links", () => {
+    function isValidUrl(string) {
+        try {
+          new URL(string);
+        } catch (_) {
+          return false;  
+        }
+      
+        return true;
+    }
+
+    console.log(
+      animatronics.every((animatronic) =>  isValidUrl(animatronic.image))
+    );
+  });
+});
