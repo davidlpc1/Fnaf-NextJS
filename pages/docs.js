6eec47a3120a2acf916ecfc90526f05eb8d434b3
Davi Lucas <davilucasdlcreator@gmail.com>
2021-03-24 19:10:21 -0300

    :bug: fix image of nightmare chica

--- pages/docs.js
+++ pages/docs.js
@@ -1,3 +1,13 @@
+import Head from '../src/components/Head';
+import Menu from '../src/components/Menu';
+
 export default function Docs() {
-  return <div>Em Construção</div>;
+  return (
+    <>
+      <Head title="Fnaf API Docs" />
+      <Menu />
+      <div style={{ padding:20 }} />
+      <h1>Fnaf API Docs</h1>
+    </>
+  )
 }
